a394dd1677f15be0d584b7fff030677bad9cf514
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-01-27 21:45:48 +0800

    Add HTML metadata tags

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
+import { Fragment } from "react";
+
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import "normalize.css";
 
 import { MantineProvider } from "@mantine/core";
@@ -8,8 +12,16 @@ import "@/styles/root.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <MantineProvider>
-      <Component {...pageProps} />
-    </MantineProvider>
+    <Fragment>
+      <Head>
+        <title>PAWAI 2026: Gods Paradise (Coming Soon)</title>
+
+        <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
+      </Head>
+
+      <MantineProvider>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </Fragment>
   );
 };
