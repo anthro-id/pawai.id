6808d62d174fa0fc14aa657a128f5305efb5f559
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-01-27 21:52:50 +0800

    Add a minimal OpenGraph metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,6 +16,12 @@ export default function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>PAWAI 2026: Gods Paradise (Coming Soon)</title>
 
+        <meta content="PAWAI 2026: Gods Paradise" property="og:title" />
+        <meta content="A long-awaited furry gathering event, nestled in the island paradise of Bali, Indonesia." property="og:description" />
+        <meta content="https://pawai.id" property="og:url" />
+        <meta content="https://canine.cdn.anthro.id/handled-by-anthroid-team/27-01-26/ff2847e9-1470-4a8a-a228-fd9877440b0f" property="og:image" />
+        <meta content="#FC5D01" name="theme-color" />
+
         <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1" />
       </Head>
 
