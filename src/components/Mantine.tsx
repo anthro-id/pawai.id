903d7f0ffdc1df8e01f2e66f2a16f079205a685c
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-02-25 22:26:50 +0800

    Apply more Mantine styles

--- src/components/Mantine.tsx
+++ src/components/Mantine.tsx
@@ -6,3 +6,8 @@ import "@mantine/core/styles/Flex.css";
 import "@mantine/core/styles/SimpleGrid.css";
 import "@mantine/core/styles/Text.css";
 import "@mantine/core/styles/Image.css";
+import "@mantine/core/styles/Title.css";
+import "@mantine/core/styles/UnstyledButton.css";
+import "@mantine/core/styles/Button.css";
+import "@mantine/core/styles/Menu.css";
+import "@mantine/core/styles/Popover.css";
