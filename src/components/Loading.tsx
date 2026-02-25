36c1d8be46ee3a0d597166ce685e3b18719907b6
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-02-25 11:34:10 +0800

    Remove legacy page

--- src/components/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Flex, Loader } from "@mantine/core";
-
-export default function Loading() {
-  return (
-    <Flex justify={"center"} align={"center"} h={256}>
-      <Loader size={"md"} />
-    </Flex>
-  );
-};
