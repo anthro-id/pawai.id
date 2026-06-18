233f7142b49b62df795b2486e1e14c87312224d5
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-06-18 12:41:06 +0800

    Deprecate server-side rendering properties in frontpage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,19 @@
-import { Fragment, lazy, type FC } from "react";
+import { Fragment, lazy } from "react";
 
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
 dayjs.extend(isBetween);
 
-import type { InferGetServerSidePropsType, GetServerSidePropsResult } from "next";
-
-import { HotelReservationContext } from "@/config";
-
 const Components = {
   Header: lazy(() => import("../components/Header")),
-  // BarongPopup: lazy(() => import("../components/frontpage/BarongPopup")),
   Hero: lazy(() => import("../components/frontpage/Hero")),
   Body: lazy(() => import("../components/frontpage/Body"))
 };
 
-const Frontpage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (props) => {
+export default function Frontpage(){
   return (
     <Fragment>
-      <HotelReservationContext value={props.hotelReservationUrl}>
-        <Components.Header />
-      </HotelReservationContext>
+      <Components.Header />
 
       <Components.Hero />
 
@@ -28,19 +21,3 @@ const Frontpage: FC<InferGetServerSidePropsType<typeof getServerSideProps>> = (p
     </Fragment>
   );
 };
-
-export function getServerSideProps(): GetServerSidePropsResult<{ hotelReservationUrl: string | null }> {
-  // "Date 2026 [] Date 2026 [] https://..."
-  const [minDateString, maxDateString, redirectUrl] =
-    (process.env.HOTEL_RESERVE_CONTENT as string).split(" [] ") as [string, string, string];
-
-  const isReservationOpen = dayjs().isBetween(dayjs(minDateString), dayjs(maxDateString));
-
-  return {
-    props: {
-      hotelReservationUrl: isReservationOpen ? redirectUrl : null
-    }
-  };
-};
-
-export default Frontpage;
