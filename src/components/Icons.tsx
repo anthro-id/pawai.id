fcb973abfbe9f3e7dd766e65c457f57d86a79896
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-09-04 19:38:10 +0800

    Add "External Link" icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -60,3 +60,14 @@ export const IconPatreon = (props: Record<"w" | "h", number>) => (
     <path d="M7.462 3.1c2.615 -1.268 6.226 -1.446 9.063 -.503c2.568 .853 4.471 3.175 4.475 5.81c.004 3.061 -1.942 5.492 -4.896 6.243c-1.693 .43 -2.338 .75 -2.942 1.582c-.238 .328 -.45 .745 -.796 1.533l-.22 .5c-1.146 2.601 -2.156 3.762 -4.236 3.735c-2.232 -.03 -3.603 -1.742 -4.313 -4.48c-.458 -1.768 -.617 -3.808 -.594 -5.876c.044 -3.993 1.42 -7.072 4.46 -8.545z" />
   </svg>
 );
+
+export const IconExternalLink = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+    <title>External Link</title>
+
+    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+    <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6" />
+    <path d="M11 13l9 -9" />
+    <path d="M15 4h5v5" />
+  </svg>
+);
