183d64d52edaac9c253aba7e6efe984b291676a2
ray-1337 <33544674+ray-1337@users.noreply.github.com>
2026-09-08 17:38:02 +0800

    Add "Maximize" icon

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -79,3 +79,17 @@ export const IconX = () => (
     <path d="M6 6l12 12" />
   </svg>
 );
+
+export const IconMaximize = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  	<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  	<path d="M16 4l4 0l0 4" />
+  	<path d="M14 10l6 -6" />
+  	<path d="M8 20l-4 0l0 -4" />
+  	<path d="M4 20l6 -6" />
+  	<path d="M16 20l4 0l0 -4" />
+  	<path d="M14 14l6 6" />
+  	<path d="M8 4l-4 0l0 4" />
+  	<path d="M4 4l6 6" />
+  </svg>
+);
